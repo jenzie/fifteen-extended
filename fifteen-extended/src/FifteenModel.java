6af86286da4df96df110d44ea6582a468f0f0485
Jenny Zhen <jxz6853@rit.edu>
2014-05-15 19:34:48 -0400

    Adding the code for the model.

---
+++ fifteen-extended/src/FifteenModel.java
@@ -0,0 +1,67 @@
+/**
+ * Model <--> ViewProxy <--> Model Listener <--> ModelProxy <--> View
+ * Direction of Communication (where ML is model listener, VL is view listener):
+ * ML (view) -> VL (ModelProxy) *-> server -> model
+ *      -> ML (ViewProxy) ->  VL (ModelProxy) -> ML (View)
+ *
+ * @author Jenny Zhen
+ * date: 05.17.14
+ * language: Java
+ * file: FifteenModel.java
+ * assignment: Fifteen
+ * http://www.cs.rit.edu/~wrc/courses/csci251/projects/4/
+ */
+
+public class FifteenModel implements FifteenViewListener {
+    public static final int WIN_SCORE = 15; // win the game with this score
+    public static final int MAX_TILES = 9; // maximum available tiles
+
+    private int tilesAvailable; // number of tiles available
+    private boolean[] board; // tiles available (true if available)
+
+    private String alpha, beta; // name of the players
+    private int alphaScore, betaScore; // scores of the players
+    private FifteenModelListener fifteenML; //
+
+    public FifteenModel(String alphaName, FifteenModelListener fifteenML) {
+        this.alpha = alphaName;
+
+        for(boolean tile : this.board)
+            tile = false;
+
+        this.tilesAvailable = MAX_TILES;
+        this.alphaScore = 0;
+        this.betaScore = 0;
+
+        this.fifteenML = fifteenML;
+    }
+
+    public void addPlayer(String betaName) {
+        this.beta = betaName;
+    }
+
+    @Override
+    public void newgame() {
+        for(boolean tile : this.board)
+            tile = false;
+
+        this.tilesAvailable = MAX_TILES;
+        this.alphaScore = 0;
+        this.betaScore = 0;
+    }
+
+    @Override
+    public void setDigit(int digit) {
+        int index = digit - 1;
+
+        if(this.board[index] == false) {
+            this.board[index] = true;
+            this.tilesAvailable--;
+        }
+    }
+
+    @Override
+    public void quit() {
+        this.fifteenML.quit();
+    }
+}
